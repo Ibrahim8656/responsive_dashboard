b750a527895611a897a782d036810e842afea268
ebrahimWaleed <ebrahimwaleed44@gmail.com>
2024-11-17 14:39:27 +0200

    first commit

---
+++ lib/Home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_application_2/widgets/Gridveiw.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey<ScaffoldState>scaffoldkey=GlobalKey();
+    return LayoutBuilder(builder: (context,constrains){
+      var mywidth=constrains.maxWidth;
+      if(constrains.maxWidth<=500){
+ return Scaffold(
+      key: scaffoldkey,
+      appBar: AppBar(
+        leading: IconButton( onPressed: () { 
+          scaffoldkey.currentState!.openDrawer();
+        }, icon: Icon(Icons.menu,)),
+      ),
+      drawer: CustomDrawer(),
+       body: CustomScrollView(
+        slivers: [
+           SliverToBoxAdapter(child: Center(child: Text("Titele",style: TextStyle(fontSize: 16*(mywidth/500)),))),
+       customgridveiw(),
+      Customlistveiw() ],
+      )
+       
+    );
+      }else return Scaffold(
+        key: scaffoldkey,
+        appBar: AppBar(
+        leading: IconButton( onPressed: () { 
+          scaffoldkey.currentState!.openDrawer();
+        }, icon: Icon(Icons.menu,)),
+      ),
+      drawer: CustomDrawer(),
+       body:
+        CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: Center(child: Text("Titele",style: TextStyle(fontSize: 16*(mywidth/500)),))),
+      SliverToBoxAdapter(
+        child: SizedBox(
+          height: 200,
+          child: ListView.builder(
+            itemBuilder: (context,index)=>Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 150,width: 150,color: Colors.grey,),
+          ),itemCount: 15,scrollDirection:Axis.horizontal,),
+        ),
+      ),
+      Customlistveiw() ],
+      )
+       );
+    },);
+   
+  }
+}
---
+++ lib/widgets/Gridveiw.dart
@@ -0,0 +1,43 @@
+
+ import 'package:flutter/material.dart';
+
+Widget  customgridveiw()=> SliverPadding(
+          padding: const EdgeInsets.all(25.0),
+          sliver: SliverGrid.builder(
+            itemCount: 4,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing:15,mainAxisSpacing: 15
+          ), itemBuilder: (context,index)=>Container(color: Colors.grey,),
+          ),
+        );
+
+        Widget Customlistveiw()=> SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Padding(
+                padding: const EdgeInsets.only(left: 25,right: 25,bottom: 15),
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              childCount: 15,
+            ),
+          );
+  Widget CustomDrawer()=>Drawer(
+        child: Column(
+          children: [
+
+            DrawerHeader(child: 
+            Icon(
+              Icons.favorite_sharp
+            ))
+          ],
+        ),
+        backgroundColor: Colors.grey,
+      );
+     
+       
+  
